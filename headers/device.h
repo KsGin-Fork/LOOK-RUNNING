2846116b8003da8164a61a90ca12801001e49ef6
KsGin <imqqyangfan@gmail.com>
2018-08-27 09:25:55 +0800

    update device class

--- headers/device.h
+++ headers/device.h
@@ -5,9 +5,31 @@
 #ifndef LOOK_RUNNING_DEVICE_H
 #define LOOK_RUNNING_DEVICE_H
 
+#define GLEW_STATIC
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 class device {
-
+public:
+    typedef GLbitfield bitfield;
+    typedef GLclampf clampf;
+    typedef GLFWkeyfun keyfun;
+private:
+    GLFWwindow *window;
+public:
+    device() = default;
+    ~device() = default;
+    bool initialize(const char *title, int width, int height);
+    bool windowShouldClosed();
+    void setWindowShouldClose(bool shouldClosed);
+    void setKeyCallback(keyfun fun);
+    void present();
+    void setViewPort(int x , int y , int w , int d);
+    void pollEvents();
+    void clear(bitfield mask);
+    void clearColor(clampf r , clampf g , clampf b , clampf a);
+    void destroy();
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,43 +1,5 @@
 #include <iostream>
-
-#define GLEW_STATIC
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
-void InitGlfw(){
-    /*初始化glfw*/
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR , 3); //设置最大版本
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR , 3); //设置最小版本
-    glfwWindowHint(GLFW_OPENGL_PROFILE , GLFW_OPENGL_CORE_PROFILE); //设置core-profile
-    glfwWindowHint(GLFW_RESIZABLE , false); //设置不可改变大小
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT , GL_TRUE);
-#endif
-}
-
-
-void DisPlayColor(){
-
-    static int r = 0;
-    static int g = 128;
-    static int b = 255;
-    static int dr = 1;
-    static int dg = 1;
-    static int db = 1;
-
-
-    glClearColor(r / 255.0f  , g / 255.0f , b / 255.0f  , 1.0f);
-
-    r += dr;
-    g += dg;
-    b += db;
-    if (r > 255 || r < 0) dr = -dr;
-    if (g > 255 || g < 0) dg = -dg;
-    if (b > 255 || b < 0) db = -db;
-
-    glClear(GL_COLOR_BUFFER_BIT);
-}
+#include "headers/device.h"
 
 void key_callback(GLFWwindow* window , int key , int scancode , int action , int mode){
     if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
@@ -47,42 +9,36 @@ void key_callback(GLFWwindow* window , int key , int scancode , int action , int
 
 int main()
 {
+    auto *d = new device;
 
-    InitGlfw();
+    d->initialize("Empty window" , 800 , 600);
+    d->setViewPort(100 , 100 , 800 , 600);
+    d->setKeyCallback(key_callback);
 
-    //创建一个窗口
-    GLFWwindow *window = glfwCreateWindow(800 , 600 , "Empty Window" , NULL , NULL);
+    while (!d->windowShouldClosed()){
+        d->pollEvents();
 
-    //设置窗口环境
-    glfwMakeContextCurrent(window);
+        static int r = 0;
+        static int g = 128;
+        static int b = 255;
 
-    if(window == NULL){
-        std::cout << "Faild to create glfw window" << std::endl;
-        glfwTerminate();
+        d->clear(GL_COLOR_BUFFER_BIT);
+        d->clearColor(r / 255.0f  , g / 255.0f , b / 255.0f  , 1.0f);
+
+        static int dr = 1;
+        static int dg = 1;
+        static int db = 1;
+        r += dr;
+        g += dg;
+        b += db;
+        if (r > 255 || r < 0) dr = -dr;
+        if (g > 255 || g < 0) dg = -dg;
+        if (b > 255 || b < 0) db = -db;
+
+        d->present();
     }
 
-    //设置glew
-    glewExperimental = GL_TRUE;
-    if(glewInit() != GLEW_OK) {
-        std::cout << "Faild to init glew" << std::endl;
-        return -1;
-    }
-
-    //设置位置
-    glViewport(100 , 100 , 800 , 600);
-
-    glfwSetKeyCallback(window , key_callback);
-
-    while (!glfwWindowShouldClose(window)){
-
-        glfwPollEvents();
-
-        DisPlayColor();
-
-
-
-        glfwSwapBuffers(window);
-    }
-
+    d->destroy();
+    delete d;
     return 0;
 }
